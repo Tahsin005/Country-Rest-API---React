564f30a35d241442091a6ba90c770abc12df6524
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-21 17:51:05 +0600

    feat(loading): add shimmer skeleton loader for country list

---
+++ src/components/ShimmerList/ShimmerList.jsx
@@ -0,0 +1,370 @@
+import { Link } from "react-router-dom";
+
+const ShimmerList = () => {
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 my-12">
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="inline-block h-full w-full rounded-lg overflow-hidden pb-6 shadow-[0_0_8px_0_rgba(0,0,0,0.2)] transition-all duration-200 hover:scale-105">
+          <div className="h-[160px] overflow-hidden bg-gray-300">
+            
+          </div>
+          <div className="">
+            <h3 className="text-xl font-bold my-2"></h3>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong>{" "}
+              
+            </p>
+            <p className="flex-grow bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+            <p className="bg-gray-300 h-4 mx-4 my-4">
+              <strong></strong> 
+            </p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ShimmerList;
