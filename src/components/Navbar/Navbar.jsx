f5701c5dad5d5c20e1f51c681280d89affe9c04b
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-23 22:08:38 +0600

    style(navbar): make navbar sticky and update branding with globe emoji

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-xl mx-auto px-8 md:px-16 lg:px-24 py-5">
+    <div className="max-w-screen-xl mx-auto px-8 md:px-16 lg:px-24 pb-5 sticky top-0 z-10 bg-base-100">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
           </div>
-          <Link to="/" className="text-xl font-bold">Around the flags</Link>
+          <Link to="/" className="sm:text-lg md:text-xl font-bold">Around the flags🌎</Link>
         </div>
         <div className="navbar-center hidden lg:flex"></div>
-        <div className="navbar-end">
+        <div className="navbar-end text-2xl">
           🏴
         </div>
       </div>
